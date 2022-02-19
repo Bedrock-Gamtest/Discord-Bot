84bb43cae805698bb4da8e07c973ef4bf9304e22
ReallyFatYoshi <86920416+ReallyFatYoshi@users.noreply.github.com>
2022-02-19 22:30:53 +0100

    .

--- src/events/showcase.js
+++ src/events/showcase.js
@@ -3,12 +3,11 @@ const { showcase } = require("../../config.json");
 module.exports = {
 	name: 'messageCreate',
 	once: false,
-    client: true,
-	async execute(client,message) {
+	async execute(message) {
         if (message.channelId!==showcase) return;
         const date = new Date();
         message.react("👍");
         message.react("👎");
-        message.startThread({name:`${message.author.username}'s Creation ${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getMilliseconds()}`});
+        message.startThread({name:`${message.author.username}'s Creation`});
     }
 };
