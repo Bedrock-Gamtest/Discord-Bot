27453d5e1412c7ad18f8b08fbe3ce6d269316d33
ReallyFatYoshi <86920416+ReallyFatYoshi@users.noreply.github.com>
2022-02-19 22:03:35 +0100

    .

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const { Client, Intents } = require("discord.js");
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [
+  Intents.FLAGS.GUILDS,
+  Intents.FLAGS.GUILD_MESSAGES,
+  Intents.FLAGS.DIRECT_MESSAGES,
+  Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+  Intents.FLAGS.GUILD_INVITES,
+  Intents.FLAGS.GUILD_WEBHOOKS,
+  Intents.FLAGS.GUILD_VOICE_STATES,
+  Intents.FLAGS.GUILD_MEMBERS,
+] });
 
 require("dotenv").config;
 const token = process.env.token;
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.warn(`Logged in as ${client.user.tag}!`);
 });
 
-require("./slashCommandHandler.js")(client, token);
+require("./src/slashCommandHandler.js")(client, token);
+require("./src/eventHandler.js")(client);
 
 const keepAlive = require("./server");
 keepAlive();
+
 // Login the bot
 client.login(token);
---
+++ src/events/showcase.js
@@ -0,0 +1,13 @@
+const {showcase} = require('../config.json');
+module.exports = {
+    event:'messageCreate',
+    client:true,
+    async execute(client,message) {
+        if (message.channelId!==showcase) return;
+        const date = new Date();
+
+        message.react("👍");
+        message.react("👎");
+        message.startThread({name:`${message.author.username}'s Creation ${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()} ${date.getHours()}/${date.getMinutes()}/${date.getMilliseconds()}`});
+    }
+}
