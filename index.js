bf29720eba087f092806ff35972a8c4983c04465
ReallyFatYoshi <86920416+ReallyFatYoshi@users.noreply.github.com>
2022-02-19 22:22:16 +0100

    .

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const { Client, Intents } = require("discord.js");
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ 
+  intents:[
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGES,
+    Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    Intents.FLAGS.GUILD_INVITES,
+    Intents.FLAGS.GUILD_WEBHOOKS,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_MEMBERS,
+  ]
+});
 
 require("dotenv").config;
 const token = process.env.token;
