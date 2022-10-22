b5d21ccab2573556a196c6994c34a18e6fc040cd
ReallyFatYoshi <86920416+ReallyFatYoshi@users.noreply.github.com>
2022-10-22 14:44:43 +0200

    Update getScripts.ts

--- src/hooks/getScripts.ts
+++ src/hooks/getScripts.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { GITHUB_PROFILE } from "../../config.json" assert { type: "json" };
 
 export default async function getScripts(): Promise<any> {
     return await axios.get("https://api.github.com/users/" + GITHUB_PROFILE + "/received_events")
